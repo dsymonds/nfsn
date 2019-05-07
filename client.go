0ac4927f5e13c2380950d061105e3844c14569c2
David Symonds <dsymonds@gmail.com>
2019-05-07 17:22:03 +1000

    Add basic client initialisation, including loading credentials.
    This loads from ~/.nfsn-api, in a format compatible with
    https://github.com/ktdreyer/python-nfsn/.

--- client.go
+++ client.go
@@ -4,3 +4,38 @@ Package nfsn contains abstractions for interacting with the NearlyFreeSpeech.NET
 https://members.nearlyfreespeech.net/wiki/API/Reference
 */
 package nfsn
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+type Client struct {
+	user, apiKey string
+}
+
+func NewClient() (*Client, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("finding user's home dir: %v", err)
+	}
+	configFile := filepath.Join(home, ".nfsn-api")
+	raw, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("reading %s: %v", configFile, err)
+	}
+	var cfg struct {
+		User string `json:"login"`
+		Key  string `json:"api-key"`
+	}
+	if err := json.Unmarshal(raw, &cfg); err != nil {
+		return nil, fmt.Errorf("parsing %s: %v", configFile, err)
+	}
+	return &Client{
+		user:   cfg.User,
+		apiKey: cfg.Key,
+	}, nil
+}
